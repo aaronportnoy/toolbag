8b242e949bbe75f5c7cc2380cd9ef87711a9042a
Jason Jones <jasonjones@arbor.net>
2014-05-18 12:24:45 -0400

    Use QtClipboard for clipboard management
    switch to using QtClipboard for clipboard management for better
    cross-platform support

--- user/bin/CopyEA.py
+++ user/bin/CopyEA.py
@@ -9,38 +9,19 @@ import idaapi
 import idc
 import ctypes
 import re
+from PySide import QtGui
 
 print "Importing CopyEA"
-
-def Paste( data ):
-  strcpy = ctypes.cdll.msvcrt.strcpy
-  ocb = ctypes.windll.user32.OpenClipboard    #Basic Clipboard functions
-  ecb = ctypes.windll.user32.EmptyClipboard
-  gcd = ctypes.windll.user32.GetClipboardData
-  scd = ctypes.windll.user32.SetClipboardData
-  ccb = ctypes.windll.user32.CloseClipboard
-  ga = ctypes.windll.kernel32.GlobalAlloc    # Global Memory allocation
-  gl = ctypes.windll.kernel32.GlobalLock     # Global Memory Locking
-  gul = ctypes.windll.kernel32.GlobalUnlock
-  GMEM_DDESHARE = 0x2000   
-  ocb(None) # Open Clip, Default task
-  ecb()
-  hCd = ga( GMEM_DDESHARE, len(data)+1 )
-  pchData = gl(hCd)
-  strcpy(ctypes.c_char_p(pchData),data)
-  gul(hCd)
-  scd(1,hCd)
-  ccb()
-
 
 def CopyEA():
   myModuleName = idc.GetInputFile()
   MyModuleShortName = re.sub(r'\.[^.]*$','',myModuleName)
   myModuleBase = idaapi.get_imagebase()
   myOffset = idc.ScreenEA() - myModuleBase
+  clippy = QtGui.QClipboard()
   pasteStr = "bp !%s + 0x%x" % (MyModuleShortName, myOffset)
   print pasteStr
-  Paste(pasteStr)
+  clippy.setText(pasteStr)
 
 
 def start_up():
